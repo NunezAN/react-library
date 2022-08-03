a4b8a243535f33c362acd4263e7a5182ba9d0cdd
NunezAN <alex101nunez@gmail.com>
2022-08-03 10:05:05 -0500

    completed book filtering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Books from "./pages/Books";
+import {books} from "./data"
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <div className="App">
         <Nav></Nav>
         <Route path="/" exact component={Home}></Route>
+        <Route path="/books" exact render={()=><Books books={books}></Books>}></Route>
         <Footer></Footer>
       </div>
     </Router>
