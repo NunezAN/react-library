c209ddd796cfbb975cc73c2b626b5eae1ba7b9f3
NunezAN <alex101nunez@gmail.com>
2022-10-05 17:29:16 -0500

    changed data pricing

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ export const books = [
     id: 1,
     title: "Pulp Fiction",
     url: "https://posters.movieposterdb.com/07_10/1994/110912/l_110912_55345443.jpg",
-    originalPrice: 49.95,
-    salePrice: 14.95,
+    originalPrice: 34.99,
+    salePrice: 32.95,
     rating: 5,
     description:
       "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
@@ -14,7 +14,7 @@ export const books = [
     id: 2,
     title: "The Godfather",
     url: "https://posters.movieposterdb.com/22_07/1972/68646/l_68646_8c811dec.jpg",
-    originalPrice: 39,
+    originalPrice: 34.99,
     salePrice: null,
     rating: 5,
     description:
@@ -24,7 +24,7 @@ export const books = [
     id: 3,
     title: "Back to the Future",
     url: "https://posters.movieposterdb.com/08_11/1985/88763/l_88763_a131c679.jpg",
-    originalPrice: 29,
+    originalPrice: 29.99,
     salePrice: null,
     rating: 4.5,
     description:
@@ -34,8 +34,8 @@ export const books = [
     id: 4,
     title: "The Shawshank Redemption",
     url: "https://posters.movieposterdb.com/05_03/1994/0111161/l_8494_0111161_3bb8e662.jpg",
-    originalPrice: 44,
-    salePrice: 19,
+    originalPrice: 34.99,
+    salePrice: 22.95,
     rating: 4.5,
     description:
       "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
@@ -44,7 +44,7 @@ export const books = [
     id: 5,
     title: "Inglourious Basterds",
     url: "https://posters.movieposterdb.com/11_03/2009/361748/l_361748_ec32ce84.jpg",
-    originalPrice: 32,
+    originalPrice: 24.99,
     salePrice: null,
     rating: 5,
     description:
@@ -54,8 +54,8 @@ export const books = [
     id: 6,
     title: "Inception",
     url: "https://posters.movieposterdb.com/10_06/2010/1375666/l_1375666_07030c72.jpg",
-    originalPrice: 70,
-    salePrice: 12.5,
+    originalPrice: 14.99,
+    salePrice: 9.95,
     rating: 5,
     description:
       "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
@@ -64,8 +64,8 @@ export const books = [
     id: 7,
     title: "The Sandlot",
     url: "https://posters.movieposterdb.com/08_03/1993/108037/l_108037_acaa1b17.jpg",
-    originalPrice: 11,
-    salePrice: 10,
+    originalPrice: 9.99,
+    salePrice: 2.99,
     rating: 3.5,
     description:
       "In the summer of 1962, a new kid in town is taken under the wing of a young baseball prodigy and his rowdy team, resulting in many adventures.",
@@ -74,8 +74,8 @@ export const books = [
     id: 8,
     title: "Star Wars: Episode IV - A New Hope",
     url: "https://posters.movieposterdb.com/12_05/1977/76759/l_76759_7dc4ecb6.jpg",
-    originalPrice: 38,
-    salePrice: 17.95,
+    originalPrice: 39.95,
+    salePrice: 34.99,
     rating: 4.5,
     description:
       "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
@@ -84,7 +84,7 @@ export const books = [
     id: 9,
     title: "The Silence of the Lambs",
     url: "https://posters.movieposterdb.com/12_04/1991/102926/l_102926_642e9c9a.jpg",
-    originalPrice: 35,
+    originalPrice: 34.99,
     salePrice: 19.95,
     rating: 4.5,
     description:
@@ -94,7 +94,7 @@ export const books = [
     id: 10,
     title: "Avengers: Endgame",
     url: "https://posters.movieposterdb.com/20_05/2019/4154796/l_4154796_2542f70b.jpg",
-    originalPrice: 40,
+    originalPrice: 29.99,
     salePrice: null,
     rating: 5,
     description:
@@ -104,8 +104,8 @@ export const books = [
     id: 11,
     title: "Deadpool",
     url: "https://posters.movieposterdb.com/15_12/2016/1431045/l_1431045_a8e99afc.jpg",
-    originalPrice: 30,
-    salePrice: 20,
+    originalPrice: 19.99,
+    salePrice: 12.99,
     rating: 4.5,
     description:
       "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks.",
@@ -114,8 +114,8 @@ export const books = [
     id: 12,
     title: "The Dark Knight",
     url: "https://posters.movieposterdb.com/08_04/2008/468569/l_468569_828524e8.jpg",
-    originalPrice: 30,
-    salePrice: 12.95,
+    originalPrice: 19.99,
+    salePrice: 9.95,
     rating: 4,
     description:
       "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
